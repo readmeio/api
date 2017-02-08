9924cd78462f1746531230d5dc980f97bacb2969
Brandon Thomas <lucky7id@gmail.com>
2017-02-08 10:44:44 -0800

    initial commit of skeleton, needs POST route, and handler

--- commands/login.js
+++ commands/login.js
@@ -1,3 +1,47 @@
-module.exports.run = function() {
-  console.log('Log in!');
+const inquirer = require('inquirer');
+const request = require('request-promise');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address'
+  }
+];
+
+const getEmail = () => {
+  console.log('To start making request, please log in');
+
+  inquirer
+    .prompt(questions)
+    .then(getToken)
+    .catch(console.log);
 }
+
+const getToken = (input) => {
+  const data = Object.assign({}, input, {
+    machine: machine.machineIdSync()
+  });
+
+  request
+    .post('http://localhost:5001/cli/login', {
+      json: true,
+      body: data
+    })
+    .then(res => {
+      writeToken(res.token);
+      if (res.message) { console.log(res.message); }
+    })
+    .catch(res => {
+      console.warn(res.error);
+    });
+};
+
+const writeToken = (token) => {
+  
+}
+
+module.exports.run = getEmail;
