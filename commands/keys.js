5dae4f6fe58a82c72ad447419525c06021d3bc87
Marc <mjcuva@gmail.com>
2017-03-19 14:18:43 -0700

    Fix keys command

--- commands/keys.js
+++ commands/keys.js
@@ -1,7 +1,13 @@
-const creds = require('../data/creds.json');
+const request = require('request-promise');
+
+const utils = require('../utils/utils');
 
 module.exports.run = () => {
-  for (const name in creds) {
-    console.log(`${name.blue}: ${creds[name]}`);
-  }
+  const jar = utils.getJar();
+  request(`${utils.BUILD_URL}/users/me`, { jar }).then((user) => {
+    const userParsed = JSON.parse(user);
+    for (const team of userParsed.teams) {
+      console.log(`${team.name.blue}: ${team.key}`);
+    }
+  });
 };
