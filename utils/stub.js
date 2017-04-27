da8d091b9a5b967697900aaac3e5090ee749eb96
Dom Harrington <dom@harrington-mail.com>
2017-04-27 15:48:55 -0700

    Bump version of build-docs to 1.1.0 to support a full description

--- utils/stub.js
+++ utils/stub.js
@@ -3,6 +3,9 @@ const api = require('api-build');
 /*
  * <<action>>: Add a one line description of your service here
  *
+ * Add a longer description of your service here. This can be as
+ * long as you need it to be and also be on multiple lines.
+ *
  * @param {string} name Name of the person
  * @throws {ValidationError} Must provide all required fields
  * @returns {Object} The created user object
