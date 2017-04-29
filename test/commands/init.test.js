08f541b5f5bdd09b723ddd6f93c97a3b393702f6
Dom Harrington <dom@harrington-mail.com>
2017-04-28 18:06:09 -0700

    Add a couple more tests for `api init`

--- test/commands/init.test.js
+++ test/commands/init.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const fs = require('fs');
 const spawnInit = require('./spawn-init');
 const { join, basename } = require('path');
 
@@ -14,10 +15,18 @@ describe('init command', () => {
 
     return spawnInit(answers).then((tmpDir) => {
       const packageJson = require(join(tmpDir, 'package.json'));
-      assert.equal(packageJson.name, basename(tmpDir));
+      const name = basename(tmpDir);
+      const main = `${name}.js`;
+      assert.equal(packageJson.name, name);
       assert.equal(packageJson.private, false);
-      assert.equal(packageJson.main, `${basename(tmpDir)}.js`);
+      assert.equal(packageJson.main, main);
       assert.equal(packageJson.version, version);
+
+      const readme = fs.readFileSync(join(tmpDir, 'readme.md'), 'utf8');
+      assert(readme.indexOf(`# ${name}`) > -1, 'Should have readme title');
+
+      const mainFile = fs.readFileSync(join(tmpDir, main), 'utf8');
+      assert(mainFile.indexOf('api.create(\'sayHello\'') > -1, 'Should have default action');
     });
   });
 });
