f98071291d5ad8928970ac3d4bad255dbdcaf02f
Dom Harrington <dom@harrington-mail.com>
2017-05-01 15:01:21 -0700

    Add `api whoami` to return the current logged in user

---
+++ test/commands/whoami.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const nock = require('nock');
+
+const whoami = require('../../commands/whoami');
+const { BUILD_URL } = require('../../utils/utils');
+const logger = require('../../utils/console');
+
+describe('`whoami` command', () => {
+  before(() => nock.disableNetConnect());
+  after(() => nock.enableNetConnect());
+
+  it('should call `/users/me`', async () => {
+    const mock = nock(BUILD_URL).get('/users/me').reply(201, { username: 'example' });
+
+    await whoami.run();
+
+    mock.done();
+
+    // Need to do this on next tick, as it logs after the response
+    process.nextTick(() => {
+      assert(logger._flush().indexOf() > -1, 'example', 'Should print the username');
+    });
+  });
+});
