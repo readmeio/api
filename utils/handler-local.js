ac9355ebe2c79454dd3057983951b5526449f0fb
Marc Cuva <mjcuva@gmail.com>
2017-08-02 20:20:00 -0700

    Fix issue with errors in async code

--- utils/handler-local.js
+++ utils/handler-local.js
@@ -3,14 +3,15 @@ const path = require('path');
 exports.go = (event, context, callback) => {
   require(`${event.entrypoint}`);
   const api = require(path.join(process.cwd(), 'node_modules/api/api'));
-  try {
-    api.actions[event.name](event.data, {
-      success: api.success(callback),
-      error: api.error,
-      log: () => {},
-    });
-  } catch (e) {
+  api.actions[event.name](event.data, {
+    success: api.success(callback),
+    error: api.error,
+    log: () => {},
+  });
+
+  // Need this to recover from async exceptions
+  process.on('uncaughtException', (e) => {
     const error = api._handlerUtils.parseErrors(event, e);
     callback(JSON.stringify(error), null);
-  }
+  });
 };
--- utils/handler.js
+++ utils/handler.js
@@ -3,16 +3,18 @@ const api = require('api');
 /* eslint-enable */
 
 exports.go = (event, context, callback) => {
-  try {
-    require(`./${event.entrypoint}`);
-    api.actions[event.name](event.data, {
-      success: api.success(callback),
-      error: api.error,
-      log: api.log,
-      getSecret: api.secrets(event.secrets),
-    });
-  } catch (e) {
+  require(`./${event.entrypoint}`);
+  api.actions[event.name](event.data, {
+    success: api.success(callback),
+    error: api.error,
+    log: api.log,
+    getSecret: api.secrets(event.secrets),
+  });
+
+  // Using this instead of try...catch to make sure async errors
+  // get caught and everything is handled in the same way
+  process.on('uncaughtException', (e) => {
     const error = api._handlerUtils.parseErrors(event, e);
     callback(JSON.stringify(error), null);
-  }
+  });
 };
