6b0c2ff58862721bf648ff68417b5af09f0c564d
Dom Harrington <dom@harrington-mail.com>
2017-08-22 13:32:55 -0700

    Add higher coverage thresholds

--- bin/posttest.sh
+++ bin/posttest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nyc report --reporter=text-summary
-nyc check-coverage --statements 30 --branches 20 --functions 10 --lines 30
+nyc check-coverage --statements 50 --branches 50 --functions 30 --lines 50
 
 if [[ $? == 1 ]]; then
   echo 'To see the test coverage report: \n\n  nyc report --reporter=html && open coverage/index.html\n';
