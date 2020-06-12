70ba47363fc10f8de20617b51b22023140c1c9fe
Jon Ursenbach <jon@ursenba.ch>
2020-06-11 17:58:50 -0700

    test: slimming down the query-auth example a bit

--- packages/httpsnippet-client-api/__tests__/__fixtures__/output/query-auth.js
+++ packages/httpsnippet-client-api/__tests__/__fixtures__/output/query-auth.js
@@ -1,6 +1,6 @@
 const sdk = require('api')('https://example.com/openapi.json');
 
-sdk.auth('a5a220e544b16e733f59e5fe681366dd').findPetsByStatus({status: 'available', accept: 'application/xml'})
+sdk.auth('a5a220e').findPetsByStatus({status: 'available', accept: 'application/xml'})
   .then(res => res.json())
   .then(res => {
     console.log(res);
