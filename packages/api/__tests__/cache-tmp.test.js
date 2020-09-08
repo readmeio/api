3c783c2a04cbb95fab56783743d29a32401db8fc
Jon Ursenbach <jon@ursenba.ch>
2020-09-08 13:37:47 -0700

    test: adding a test for storing caches in the temp directory

---
+++ packages/api/__tests__/cache-tmp.test.js
@@ -0,0 +1,17 @@
+jest.mock('find-cache-dir', () => {
+  return () => undefined;
+});
+
+const os = require('os');
+const Cache = require('../src/cache');
+
+// Since this test is mocking out the `find-cache-dir` module for a single test, it needs to be run separately from the
+// rest of the cache tests, otherwise all of those tests would use this mocked out version.
+test('should fallback to an os-level temp directory if a cache directory cannot be determined', () => {
+  const dir = os.tmpdir();
+  const cacheStore = new Cache('http://example.com/readme.json');
+
+  expect(cacheStore.dir).toMatch(dir);
+  expect(cacheStore.cacheStore).toMatch(dir);
+  expect(cacheStore.specsCache).toMatch(dir);
+});
