4cd98ac402b0fb63daaea1c33c1aaa7ee75d7ce8
Jon Ursenbach <jon@ursenba.ch>
2022-02-23 15:44:23 -0800

    chore: fixing eslint issues

--- packages/cli/src/commands/hello/world.ts
+++ packages/cli/src/commands/hello/world.ts
@@ -1,19 +1,19 @@
-import {Command} from '@oclif/core'
+import { Command } from '@oclif/core';
 
 export default class World extends Command {
-  static description = 'Say hello world'
+  static description = 'Say hello world';
 
   static examples = [
     `$ oex hello world
 hello world! (./src/commands/hello/world.ts)
 `,
-  ]
+  ];
 
-  static flags = {}
+  static flags = {};
 
-  static args = []
+  static args = [];
 
   async run(): Promise<void> {
-    this.log('hello world! (./src/commands/hello/world.ts)')
+    this.log('hello world! (./src/commands/hello/world.ts)');
   }
 }
--- packages/cli/test/helpers/init.js
+++ packages/cli/test/helpers/init.js
@@ -1,6 +1,7 @@
-const path = require('path')
-process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json')
-process.env.NODE_ENV = 'development'
+const path = require('path');
 
-global.oclif = global.oclif || {}
-global.oclif.columns = 80
+process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json');
+process.env.NODE_ENV = 'development';
+
+global.oclif = global.oclif || {};
+global.oclif.columns = 80;
