aa6ca853bff36103296d49b8c50e79f598b6f229
Jon Ursenbach <jon@ursenba.ch>
2022-07-01 16:27:05 -0700

    fix: quirks with npm@8 installs in unit tests

--- packages/api/test/cli/codegen/languages/typescript.test.ts
+++ packages/api/test/cli/codegen/languages/typescript.test.ts
@@ -1,9 +1,9 @@
 import chai, { expect } from 'chai';
+import fs from 'fs';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import chaiPlugins from '../../../helpers/chai-plugins';
 
-import uniqueTempDir from 'unique-temp-dir';
 import Oas from 'oas';
 import Storage from '../../../../src/cli/storage';
 import TSGenerator from '../../../../src/cli/codegen/languages/typescript';
@@ -14,29 +14,38 @@ chai.use(sinonChai);
 describe('typescript', function () {
   describe('#installer', function () {
     beforeEach(function () {
-      Storage.setStorageDir(uniqueTempDir());
+      /**
+       * Create a unique temporary local directory here so we can test package installation. We need
+       * to do this instead of using something like `unique-temp-dir` because there's a bug in npm@8
+       * where if a package is installed to a non-local directory it'll throw an exception when
+       * attempting to install it as a `file:` dependency.
+       * @see {@link https://github.com/npm/cli/issues/3847}
+       */
+      Storage.setStorageDir(Math.random().toString(36).substring(2));
     });
 
-    afterEach(function () {
+    afterEach(async function () {
       Storage.reset();
+      await fs.promises.rm(Storage.getAPIsDir(), { recursive: true });
+      await fs.promises.rm(Storage.dir, { recursive: true });
     });
 
     it('should install a `package.json` and the required packages', async function () {
       const logger = sinon.spy();
 
-      const file = require.resolve('@readme/oas-examples/3.0/json/readme.json');
+      const file = require.resolve('@readme/oas-examples/3.0/json/petstore.json');
 
       const oas = await import(file).then(Oas.init);
       await oas.dereference({ preserveRefAsJSONSchemaTitle: true });
 
-      const storage = new Storage(file, 'readme');
+      const storage = new Storage(file, 'petstore');
       await storage.load();
 
-      const ts = new TSGenerator(oas, './simple.oas.json');
+      const ts = new TSGenerator(oas, './petstore.json');
       await ts.installer(storage, { logger, dryRun: true });
 
       expect(logger).to.be.calledWith(`npm install api oas --save --dry-run`);
-      expect(logger).to.be.calledWith(`npm install ${Storage.dir}/apis/readme --dry-run`);
+      expect(logger).to.be.calledWith(`npm install ${Storage.dir}/apis/petstore --dry-run`);
     });
   });
 
