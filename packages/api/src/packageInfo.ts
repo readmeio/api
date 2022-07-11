527e8c940b8b3c09f289b6e1c75822ba0b8018c8
Jon Ursenbach <jon@ursenba.ch>
2022-07-11 10:43:37 -0700

    fix: making sure that packageInfo file updates get committed on release

--- packages/api/src/packageInfo.ts
+++ packages/api/src/packageInfo.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by the build script.
 export const PACKAGE_NAME = 'api';
-export const PACKAGE_VERSION = '5.0.0-beta.0';
+export const PACKAGE_VERSION = '5.0.0-beta.1';
