9b430938349b9cd1d9f0235d24b641341de56d2a
Jon Ursenbach <jon@ursenba.ch>
2022-07-19 12:20:07 -0700

    chore: updating packageInfo

--- packages/api/src/packageInfo.ts
+++ packages/api/src/packageInfo.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by the build script.
 export const PACKAGE_NAME = 'api';
-export const PACKAGE_VERSION = '5.0.0-beta.1';
+export const PACKAGE_VERSION = '5.0.0-beta.2';
