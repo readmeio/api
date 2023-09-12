387320a2aaf9c08c38a96185de28615f9165db99
Jon Ursenbach <jon@ursenba.ch>
2023-09-11 17:50:23 -0700

    feat: deprecating the dynamic `api` entrypoint

--- packages/api/test/core/index.test.ts
+++ packages/api/test/core/index.test.ts
@@ -1,5 +1,6 @@
 import assert from 'assert';
 
+import datauri from 'datauri';
 import fetchMock from 'fetch-mock';
 import Oas from 'oas';
 import { describe, beforeEach, afterEach, it, expect } from 'vitest';
@@ -10,6 +11,8 @@ import { responses as mockResponse } from '../helpers/fetch-mock';
 import loadSpec from '../helpers/load-spec';
 
 describe('APICore', () => {
+  let fileUploads: APICore;
+  let parametersStyle: APICore;
   let petstore: APICore;
   let readme: APICore;
   let security: APICore;
@@ -22,6 +25,14 @@ describe('APICore', () => {
   };
 
   beforeEach(async () => {
+    fileUploads = await loadSpec('@readme/oas-examples/3.0/json/file-uploads.json')
+      .then(Oas.init)
+      .then(oas => new APICore(oas));
+
+    parametersStyle = await loadSpec('@readme/oas-examples/3.1/json/parameters-style.json')
+      .then(Oas.init)
+      .then(oas => new APICore(oas));
+
     petstore = await loadSpec('@readme/oas-examples/3.0/json/petstore-expanded.json')
       .then(Oas.init)
       .then(oas => new APICore(oas));
@@ -193,6 +204,107 @@ describe('APICore', () => {
           });
         });
       });
+
+      describe('application/x-www-form-urlencoded', () => {
+        it('should support `application/x-www-form-urlencoded` requests', async () => {
+          const usptoSpec = await loadSpec('@readme/oas-examples/3.0/json/uspto.json')
+            .then(spec => {
+              // eslint-disable-next-line no-param-reassign
+              spec.servers[0].url = '{scheme}://httpbin.org/anything';
+              return spec;
+            })
+            .then(Oas.init)
+            .then(oas => new APICore(oas));
+
+          fetchMock.post('https://httpbin.org/anything/v1/oa_citations/records', mockResponse.all);
+
+          const body = {
+            criteria: 'propertyName:value',
+          };
+
+          const metadata = {
+            dataset: 'v1',
+            version: 'oa_citations',
+          };
+
+          const { data } = await usptoSpec.fetch('/{dataset}/{version}/records', 'post', body, metadata);
+          expect(data.uri).toBe('/anything/v1/oa_citations/records');
+          expect(data.requestBody).toBe('criteria=propertyName%3Avalue');
+          expect(data.headers).toHaveProperty('content-type', 'application/x-www-form-urlencoded');
+        });
+      });
+
+      describe('multipart/form-data', () => {
+        it('should support `image/png` requests', async () => {
+          fetchMock.post('https://httpbin.org/anything/image-png', mockResponse.datauri);
+
+          const file = `${__dirname}/../__fixtures__/owlbert.png`;
+
+          const { data } = await fileUploads.fetch('/anything/image-png', 'post', file);
+
+          expect(data.uri).toBe('/anything/image-png');
+          expect(data.requestBody).toBe(await datauri(file));
+          expect(data.headers).toHaveProperty('content-type', 'image/png');
+        });
+
+        it('should support `multipart/form-data` requests', async () => {
+          fetchMock.post('https://httpbin.org/anything/form-data/form', mockResponse.multipart);
+
+          const body = {
+            primitive: 'string',
+            array: ['string'],
+            object: {
+              foo: 'foo-string',
+              bar: 'bar-string',
+            },
+          };
+
+          const { data } = await parametersStyle.fetch('/anything/form-data/form', 'post', body);
+          expect(data.uri).toBe('/anything/form-data/form');
+          expect(data.requestBody.split(`${data.boundary}`).filter(Boolean)).toStrictEqual([
+            '\r\nContent-Disposition: form-data; name="primitive"\r\n\r\nstring\r\n',
+            '\r\nContent-Disposition: form-data; name="array"\r\n\r\nstring\r\n',
+            '\r\nContent-Disposition: form-data; name="object"\r\n\r\nfoo,foo-string,bar,bar-string\r\n',
+            '--',
+          ]);
+        });
+
+        describe('files', () => {
+          it('should support plaintext files', async () => {
+            fetchMock.post('https://httpbin.org/anything/multipart-formdata', mockResponse.multipart);
+
+            const body = {
+              orderId: 1234,
+              userId: 5678,
+              documentFile: `${__dirname}/../__fixtures__/hello.txt`,
+            };
+
+            const { data } = await fileUploads.fetch('/anything/multipart-formdata', 'post', body);
+            expect(data.uri).toBe('/anything/multipart-formdata');
+            expect(data.requestBody.split(`${data.boundary}`).filter(Boolean)).toStrictEqual([
+              '\r\nContent-Disposition: form-data; name="orderId"\r\n\r\n1234\r\n',
+              '\r\nContent-Disposition: form-data; name="userId"\r\n\r\n5678\r\n',
+              '\r\nContent-Disposition: form-data; name="documentFile"; filename="hello.txt"\r\nContent-Type: text/plain\r\n\r\nHello world!\n\r\n',
+              '--',
+            ]);
+          });
+
+          it('should support plaintext files containing unicode characters', async () => {
+            fetchMock.post('https://httpbin.org/anything/multipart-formdata', mockResponse.multipart);
+
+            const body = {
+              documentFile: `${__dirname}/../__fixtures__/hello.jp.txt`,
+            };
+
+            const { data } = await fileUploads.fetch('/anything/multipart-formdata', 'post', body);
+            expect(data.uri).toBe('/anything/multipart-formdata');
+            expect(data.requestBody.split(`${data.boundary}`).filter(Boolean)).toStrictEqual([
+              '\r\nContent-Disposition: form-data; name="documentFile"; filename="hello.jp.txt"\r\nContent-Type: text/plain\r\n\r\n速い茶色のキツネは怠惰な犬を飛び越えます\n\r\n',
+              '--',
+            ]);
+          });
+        });
+      });
     });
   });
 
@@ -251,5 +363,39 @@ describe('APICore', () => {
     it.todo('should be able to supply a url on an OAS that has no servers defined');
 
     it.todo("should be able to supply a url that doesn't match any defined server");
+  });
+
+  describe('#setConfig()', () => {
+    describe('timeout', () => {
+      let petstoreTimeout: APICore;
+
+      beforeEach(async () => {
+        petstoreTimeout = await loadSpec('@readme/oas-examples/3.0/json/petstore.json')
+          .then(Oas.init)
+          .then(oas => new APICore(oas));
+      });
+
+      afterEach(() => {
+        fetchMock.restore();
+      });
+
+      it('should override the default `fetch` timeout if present and fail if request takes too long', async () => {
+        fetchMock.delete(`http://petstore.swagger.io/v2/pet/${petId}`, mockResponse.delay(response, 500));
+
+        petstoreTimeout.setConfig({ timeout: 100 });
+
+        await expect(petstoreTimeout.fetch(`/pet/${petId}`, 'delete')).rejects.toThrow('The operation was aborted.');
+      });
+
+      it('should override the default `fetch` timeout and return if request is quick', async () => {
+        fetchMock.delete(`http://petstore.swagger.io/v2/pet/${petId}`, mockResponse.delay(response, 100));
+
+        petstoreTimeout.setConfig({ timeout: 500 });
+
+        await petstoreTimeout.fetch(`/pet/${petId}`, 'delete').then(({ data }) => {
+          expect(data).toStrictEqual(response);
+        });
+      });
+    });
   });
 });
