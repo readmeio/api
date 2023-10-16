b1e705dddedd223f360f1dfe620ae721f54af88b
Jon Ursenbach <jon@ursenba.ch>
2023-10-16 12:34:16 -0700

    v7.0.0-alpha.5

--- packages/api/src/packageInfo.ts
+++ packages/api/src/packageInfo.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by the build script.
 export const PACKAGE_NAME = 'api';
-export const PACKAGE_VERSION = '7.0.0-alpha.3';
+export const PACKAGE_VERSION = '7.0.0-alpha.5';
