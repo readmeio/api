ae63ff5306a6368c9eaf1879038d7396d0c6c08a
Jon Ursenbach <jon@ursenba.ch>
2023-10-25 16:09:57 -0700

    refactor(snippets): moving the library over to be an HTTPSnippet plugin

--- packages/httpsnippet-client-api/test/index.test.ts
+++ packages/httpsnippet-client-api/test/index.test.ts
@@ -1,17 +1,17 @@
 import type { HarRequest, Request } from '@readme/httpsnippet';
-import type { Client } from '@readme/httpsnippet/targets';
+import type { ClientPlugin } from '@readme/httpsnippet/targets';
 import type { OASDocument } from 'oas/rmoas.types';
 
 import { readdirSync } from 'node:fs';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-import { HTTPSnippet, addTargetClient } from '@readme/httpsnippet';
+import { HTTPSnippet, addClientPlugin } from '@readme/httpsnippet';
 import readme from '@readme/oas-examples/3.0/json/readme.json';
 import openapiParser from '@readme/openapi-parser';
 import { describe, beforeEach, expect, it } from 'vitest';
 
-import client from '../src/index.js';
+import plugin from '../src/index.js';
 
 const DATASETS_DIR = path.join(__dirname, '__datasets__');
 const SNIPPETS = readdirSync(DATASETS_DIR);
@@ -28,7 +28,7 @@ function getSnippetDataset(snippet): Promise<SnippetMock> {
 describe('httpsnippet-client-api', () => {
   beforeEach(() => {
     try {
-      addTargetClient('node', client as Client);
+      addClientPlugin(plugin as ClientPlugin);
     } catch (err) {
       if (err.message !== 'The supplied custom target client already exists, please use a different key') {
         throw err;
@@ -37,36 +37,54 @@ describe('httpsnippet-client-api', () => {
   });
 
   it('should have info', () => {
-    expect(client).toHaveProperty('info');
-    expect(client.info).toStrictEqual({
+    expect(plugin).toHaveProperty('target', 'node');
+    expect(plugin.client).toHaveProperty('info');
+    expect(plugin.client.info).toStrictEqual({
       key: 'api',
       title: 'API',
       link: 'https://npm.im/api',
       description: 'Automatic SDK generation from an OpenAPI definition.',
       extname: '.js',
+      installation: 'npx api install {packageName}',
     });
   });
 
-  it('should error if no apiDefinitionUri was supplied', async () => {
+  it('should error if no `api` config was supplied', async () => {
     const { har } = await getSnippetDataset('petstore');
     const snippet = new HTTPSnippet(har);
 
-    await expect(snippet.convert('node', 'api')).rejects.toThrow(/must have an `apiDefinitionUri` option supplied/);
+    await expect(snippet.convert('node', 'api')).rejects.toThrow(/must have an `api` config supplied/);
+    await expect(snippet.convert('node', 'api', {})).rejects.toThrow(/must have an `api` config supplied/);
   });
 
-  it('should error if no apiDefinition was supplied', async () => {
+  it('should error if no `api.definition` was supplied', async () => {
     const { har } = await getSnippetDataset('petstore');
     const snippet = new HTTPSnippet(har);
 
     await expect(
       snippet.convert('node', 'api', {
-        apiDefinitionUri: '@developers/v2.0#17273l2glm9fq4l5',
+        api: {
+          registryURI: '@developers/v2.0#17273l2glm9fq4l5',
+        },
       }),
-    ).rejects.toThrow(/must have an `apiDefinition` option supplied/);
+    ).rejects.toThrow(/must have an `api.definition` option supplied/);
+  });
+
+  it('should error if no `api.registryURI` was supplied', async () => {
+    const { har } = await getSnippetDataset('petstore');
+    const snippet = new HTTPSnippet(har);
+
+    await expect(
+      snippet.convert('node', 'api', {
+        api: {
+          definition: readme,
+        },
+      }),
+    ).rejects.toThrow(/must have an `api.registryURI` option supplied/);
   });
 
   // This test should fail because the url in the HAR is missing `/v1` in the path.
-  it('should error if no matching operation was found in the apiDefinition', async () => {
+  it('should error if no matching operation was found in the supplied API definition', async () => {
     const har = {
       httpVersion: 'HTTP/1.1',
       method: 'GET',
@@ -81,8 +99,10 @@ describe('httpsnippet-client-api', () => {
 
     await expect(
       snippet.convert('node', 'api', {
-        apiDefinitionUri: '@developers/v2.0#17273l2glm9fq4l5',
-        apiDefinition: readme,
+        api: {
+          definition: readme,
+          registryURI: '@developers/v2.0#17273l2glm9fq4l5',
+        },
       }),
     ).rejects.toThrow(/unable to locate a matching operation/i);
   });
@@ -104,8 +124,10 @@ describe('httpsnippet-client-api', () => {
         const expected = await fs.readFile(path.join(DATASETS_DIR, snippet, 'output.js'), 'utf-8');
 
         const code = await new HTTPSnippet(mock.har).convert('node', 'api', {
-          apiDefinitionUri: `@${snippet}/v2.0#17273l2glm9fq4l5`,
-          apiDefinition: mock.definition,
+          api: {
+            definition: mock.definition,
+            registryURI: `@${snippet}/v2.0#17273l2glm9fq4l5`,
+          },
         });
 
         expect(`${code}\n`).toStrictEqual(expected);
@@ -117,9 +139,11 @@ describe('httpsnippet-client-api', () => {
         const mock = await getSnippetDataset('petstore');
 
         const code = await new HTTPSnippet(mock.har).convert('node', 'api', {
-          apiDefinitionUri: '@developers/v2.0#17273l2glm9fq4l5',
-          identifier: 'developers',
-          apiDefinition: mock.definition,
+          api: {
+            definition: mock.definition,
+            identifier: 'developers',
+            registryURI: '@developers/v2.0#17273l2glm9fq4l5',
+          },
         });
 
         expect(code).toStrictEqual(`import developers from '@api/developers';
@@ -134,9 +158,11 @@ developers.findPetsByStatus({status: 'available', accept: 'application/xml'})
         const mock = await getSnippetDataset('petstore');
 
         const code = await new HTTPSnippet(mock.har).convert('node', 'api', {
-          apiDefinitionUri: '@metro-transit/v2.0#17273l2glm9fq4l5',
-          identifier: 'metro-transit',
-          apiDefinition: mock.definition,
+          api: {
+            definition: mock.definition,
+            identifier: 'metro-transit',
+            registryURI: '@metro-transit/v2.0#17273l2glm9fq4l5',
+          },
         });
 
         expect(code).toStrictEqual(`import metroTransit from '@api/metro-transit';
