eecc27f4af7e26f98c12f1c459f06b5b96a0efec
Kanad Gupta <git@kanad.dev>
2023-10-26 10:44:44 -0500

    feat: throw error for legacy dynamic `require` users

---
+++ packages/api/legacy-require-handler.cjs
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-commonjs */
+
+/**
+ * You can regenerate the type declarations for this file by running the following:
+ * ```sh
+ * npx tsc legacy-require-handler.cjs --checkJs --declaration --emitDeclarationOnly
+ * ```
+ */
+
+/**
+ * In `api` v6, this package was a JS library used JavaScript Proxies to dynamically generate SDKs at runtime.
+ * This functionality has been sunset in `api` v7 to focus on its CLI, which builds strongly-typed SDKs.
+ * This file is here to throw errors for users that attempt to use the legacy library.
+ *
+ *
+ * @param {unknown} input
+ */
+module.exports = function legacyHandler(input) {
+  throw new Error(`Oops! You're attempting to use a legacy usage pattern that no longer exists.
+
+You can read more about this in our docs: TKTK
+
+\`api\` can now generate strongly-typed SDKs that work in pretty much every JS runtime!
+
+Try running this command to get started:
+
+npx api@latest install ${input}`);
+};
---
+++ packages/api/legacy-require-handler.d.cts
@@ -0,0 +1,2 @@
+declare function _exports(input: unknown): never;
+export = _exports;
