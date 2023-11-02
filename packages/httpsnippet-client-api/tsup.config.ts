aa49eb585a7b2c6d4b58aa749b09881122be932f
Jon Ursenbach <erunion@users.noreply.github.com>
2023-11-02 11:46:01 -0700

    fix(httpsnippet-client-api): making external deps no longer external (#810)
    * fix: marking external esm-only deps as bundled deps
    
    * fix: moving camelcase and stringify-object to being devdeps
    
    * fix: dont use bundledDependencies

--- packages/httpsnippet-client-api/tsup.config.ts
+++ packages/httpsnippet-client-api/tsup.config.ts
@@ -11,5 +11,15 @@ export default defineConfig((options: Options) => ({
   ...config,
 
   entry: ['src/index.ts'],
+
+  noExternal: [
+    // These dependencies are ESM-only but because we're building for ESM **and** CJS we can't
+    // treat them as external dependencies as CJS libraries can't load ESM code that uses `export`.
+    // `noExternal` will instead treeshake these dependencies down and include them in our compiled
+    // dists.
+    'camelcase',
+    'stringify-object',
+  ],
+
   silent: !options.watch,
 }));
