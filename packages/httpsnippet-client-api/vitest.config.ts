b575d9024e0110cc78df1fd1b9f9c263ccf56c31
Jon Ursenbach <jon@ursenba.ch>
2025-02-01 00:11:21 -0800

    feat: consolidating into a single vitest config

--- packages/httpsnippet-client-api/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    exclude: ['dist', 'node_modules', '**/__datasets__/**', '**/__fixtures__/**', '**/helpers/**'],
-  },
-});
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,46 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    workspace: ['packages/api', 'packages/core', 'packages/httpsnippet-client-api']
+    env: {
+      // Vitest strips colors from content by default and `chalk` has troubles with color detection
+      // in CI.
+      // https://github.com/chalk/supports-color/issues/106
+      FORCE_COLOR: '1',
+    },
+    exclude: [
+      '**/__fixtures__/**',
+      '**/__datasets__/**',
+      '**/datasets/**',
+      '**/dist/**',
+      '**/helpers/**',
+      '**/node_modules/**',
+    ],
+    setupFiles: ['packages/test-utils/vitest.matchers.ts'],
+    workspace: [
+      {
+        extends: true,
+        test: {
+          exclude: [
+            '**/smoketest.test.ts',
+          ],
+          include: ['packages/api/**/*.test.ts'],
+          name: 'api',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          include: ['packages/core/**/*.test.ts'],
+          name: 'core',
+        }
+      },
+      {
+        extends: true,
+        test: {
+          include: ['packages/httpsnippet-client-api/**/*.test.ts'],
+          name: 'httpsnippet-client-api',
+        }
+      },
+    ],
   },
 });
