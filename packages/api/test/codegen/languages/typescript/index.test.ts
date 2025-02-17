efd188f4ec8772d66e836a5c63edde1aa7897020
Jon Ursenbach <jon@ursenba.ch>
2025-02-17 10:50:00 -0800

    fix: bumping up the test timeout for the star trek test

--- packages/api/test/codegen/languages/typescript/index.test.ts
+++ packages/api/test/codegen/languages/typescript/index.test.ts
@@ -173,6 +173,7 @@ describe('typescript', () => {
 
     it(
       'should work against a very large API definition that has `default` responses',
+      { timeout: 10000 },
       assertSDKFixture('@readme/oas-examples/3.0/json/star-trek.json', 'star-trek'),
     );
 
