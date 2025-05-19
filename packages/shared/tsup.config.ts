eade1f78d9e2f58b1462855f380a27a6e37595b9
Andrii Andreiev <129078694+AndriiAndreiev@users.noreply.github.com>
2025-05-19 16:39:49 +0300

    fix: attw issues (package.json)

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -10,14 +10,15 @@ export default defineConfig((options: Options) => ({
   ...options,
   ...config,
 
-  entry: ['src/errors/fetchError.ts', 'src/lib/index.ts', 'src/index.ts', 'src/types.ts'],
+  entry: ['src/index.ts'],
 
   noExternal: [
     // These dependencies are ESM-only but because we're building for ESM **and** CJS we can't
     // treat them as external dependencies as CJS libraries can't load ESM code that uses `export`.
     // `noExternal` will instead treeshake these dependencies down and include them in our compiled
     // dists.
-    'get-stream',
+    'camelcase',
+    'stringify-object',
   ],
 
   silent: !options.watch,
