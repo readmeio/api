1788e4aefcc207787fe2b39b3f43e7c12c8ac5a6
Jon Ursenbach <jon@ursenba.ch>
2026-07-23 11:05:47 -0700

    fix: updating `PACKAGE_VERSION` in the package info file

--- packages/api/src/packageInfo.ts
+++ packages/api/src/packageInfo.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by the build script.
 export const PACKAGE_NAME = 'api';
-export const PACKAGE_VERSION = '7.0.0-next.18';
+export const PACKAGE_VERSION = '7.0.0';
